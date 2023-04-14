24804f888f7f00f78374a4d92cd2e9ef5740b106
nguyenducde <ducde678@gmail.com>
2023-04-14 13:19:18 +0700

    fix baseUrl

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,5 @@
 const baseUrlLocal = "http://localhost:5000";
 
-const baseUrl = process.env.REACT_APP_API_URL;
+// const baseUrl = process.env.REACT_APP_API_URL;
+const baseUrl = "https://blogapi-s3q3.onrender.com";
 export default baseUrl;
